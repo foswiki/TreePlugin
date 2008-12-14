80331549d27e420124436e32edb3358353ddd597
StephaneLenclud <StephaneLenclud@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-12-14 21:31:00 +0000

    Item467: TreePlugin port to Foswiki. build.pl fix.
    git-svn-id: http://svn.foswiki.org/trunk/TreePlugin@1347 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/TreePlugin/build.pl
+++ lib/Foswiki/Plugins/TreePlugin/build.pl
@@ -8,10 +8,10 @@ BEGIN {
   }
 }
 
-use Foswiki::::Contrib::Build;
+use Foswiki::Contrib::Build;
 
 # Create the build object
-$build = new Foswiki::::Contrib::Build( 'TreePlugin' );
+$build = new Foswiki::Contrib::Build( 'TreePlugin' );
 
 # Build the target on the command line, or the default target
 $build->build($build->{target});
